0b1f00f96f531ff5300b5ca884280e263228e111
zhang165 <zhang165@gmail.com>
2015-12-06 21:12:25 -0800

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+$( document ).ready(function() {
+
+	// scroll functionality
+	var percent = 0.05;
+	var header = $('#banner');
+	var title = $('#title');
+
+	// scroll functionality
+	$(window).scroll(function(){
+    var scrollTop = $(this).scrollTop();
+    var height = $(document).height();
+    
+    console.log(5*scrollTop/height);
+
+    var calc = 1 - (5*scrollTop/height);
+
+    if(calc +0.4 < percent){
+    	title.addClass('smaller');
+    }else if(calc >= percent){
+    	title.removeClass('smaller');
+    }
+
+    header.css({ 'opacity': calc });  
+    if ( calc > '1' ) {
+      header.css({ 'opacity': 1 });
+    } else if ( calc < '0' ) {
+      header.css({ 'opacity': 0 });
+    }
+
+    }); 
+
+});
+
