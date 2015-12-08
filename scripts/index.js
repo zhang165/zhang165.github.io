5bcde36596de02ab3b2771877d3f6a75d702d4b3
zhang165 <zhang165@gmail.com>
2015-12-08 11:56:21 -0800

    testing new scroll functionality

--- scripts/index.js
+++ scripts/index.js
@@ -6,44 +6,37 @@ $( document ).ready(function() {
 	var percent = 0.05;
 	var header = $('#banner');
 	var title = $('#title');
-	
-	$(window).scroll(function(){
-    		var scrollTop = $(this).scrollTop();
-    		var height = $(document).height();
 
-    var calc = 1 - (5*scrollTop/height);
-
-    if(calc +0.4 < percent){
-    	title.addClass('smaller');
-    }else if(calc >= percent){
-    	title.removeClass('smaller');
-    }
-
-    header.css({ 'opacity': calc });  
-    if ( calc > '1' ) {
-      header.css({ 'opacity': 1 });
-    } else if ( calc < '0' ) {
-      header.css({ 'opacity': 0 });
-    }
-
-    }); 
-
-    $.fn.moveIt = function(){
+     $.fn.moveIt = function(){
         var $window = $(window);
         var instances = [];
-  
+        
         $(this).each(function(){
             instances.push(new moveItItem($(this)));
         });
-  
         window.onscroll = function(){
         var scrollTop = $window.scrollTop();
+        var height = $(document).height();
+
+        var calc = 1 - (5*scrollTop/height);
+        if(calc +0.4 < percent){
+            title.addClass('smaller');
+        }else if(calc >= percent){
+            title.removeClass('smaller');
+        }
+
+        header.css({ 'opacity': calc });  
+
+        if ( calc > '1' ) {
+            header.css({ 'opacity': 1 });
+        } else if ( calc < '0' ) {
+            header.css({ 'opacity': 0 });
+        }
         instances.forEach(function(inst){
             inst.update(scrollTop);
             });
             }
         }
-
     var moveItItem = function(el){
         this.el = $(el);
         this.speed = parseInt(this.el.attr('data-scroll-speed'));
@@ -51,12 +44,19 @@ $( document ).ready(function() {
 
     moveItItem.prototype.update = function(scrollTop){
     var pos = scrollTop / this.speed;
-        this.el.css('transform', 'translateY(' + -pos + 'px)');
+        this.el.css({'margin-top' : -(pos/2)+"px"});
     };
 
     $(function(){
         $('[data-scroll-speed]').moveIt();
     });
+	
+	$(window).scroll(function(){
+  
+
+    }); 
+
+   
 
 });
 
