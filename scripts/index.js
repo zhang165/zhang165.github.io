41de906c304368769abef826f5e1cb98f222b9c4
zhang165 <zhang165@gmail.com>
2015-12-08 11:10:15 -0800

    testing new scroll functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 $( document ).ready(function() {
-
+    // activates fancy box
     $(".fancybox").fancybox();
 
 	// scroll functionality
 	var percent = 0.05;
 	var header = $('#banner');
 	var title = $('#title');
-	// scroll functionality
+	
 	$(window).scroll(function(){
     		var scrollTop = $(this).scrollTop();
     		var height = $(document).height();
@@ -27,6 +27,36 @@ $( document ).ready(function() {
     }
 
     }); 
+
+    $.fn.moveIt = function(){
+        var $window = $(window);
+        var instances = [];
+  
+        $(this).each(function(){
+            instances.push(new moveItItem($(this)));
+        });
+  
+        window.onscroll = function(){
+        var scrollTop = $window.scrollTop();
+        instances.forEach(function(inst){
+            inst.update(scrollTop);
+            });
+            }
+        }
+
+    var moveItItem = function(el){
+        this.el = $(el);
+        this.speed = parseInt(this.el.attr('data-scroll-speed'));
+    };
+
+    moveItItem.prototype.update = function(scrollTop){
+    var pos = scrollTop / this.speed;
+        this.el.css('transform', 'translateY(' + -pos + 'px)');
+    };
+
+    $(function(){
+        $('[data-scroll-speed]').moveIt();
+    });
 
 });
 
